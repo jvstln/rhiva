2b531817661c00f911b0256a3b277e90f313b575
Include caleb.fun <70612830+oasisMystre@users.noreply.github.com>
2026-07-30 15:57:11 +0100

    feat: bug fix

--- apps/www/src/providers/index.tsx
+++ apps/www/src/providers/index.tsx
@@ -1,5 +1,7 @@
 "use client";
+import { clusterApiUrl } from "@solana/web3.js";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ConnectionProvider } from "@solana/wallet-adapter-react";
 
 import { PrivyProvider } from "./PrivyProvider";
 import { Toaster } from "@/components/ui/sonner";
@@ -10,15 +12,17 @@ import { TooltipProvider } from "./ToolTipProvider";
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <PrivyProvider>
-          <UserApiProvider>{children}</UserApiProvider>
-          <Toaster
-            position="top-center"
-            richColors
-          />
-        </PrivyProvider>
-      </TooltipProvider>
+      <ConnectionProvider endpoint={clusterApiUrl("mainnet-beta")}>
+        <TooltipProvider>
+          <PrivyProvider>
+            <UserApiProvider>{children}</UserApiProvider>
+            <Toaster
+              position="top-center"
+              richColors
+            />
+          </PrivyProvider>
+        </TooltipProvider>
+      </ConnectionProvider>
     </QueryClientProvider>
   );
 }
